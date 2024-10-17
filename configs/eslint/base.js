3a7e5d485b8a0af72524822d2e217470a11e8ff6
Leif Arriens <leif.arriens@gmail.com>
2024-10-17 21:38:11 +0200

    refactor: mv ts & eslint config to configs packages

--- configs/eslint/base.js
+++ configs/eslint/base.js
@@ -1,22 +1,28 @@
 // @ts-check
 
 import eslint from "@eslint/js";
+import perfectionist from "eslint-plugin-perfectionist";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   ...tseslint.configs.stylisticTypeChecked,
+  perfectionist.configs["recommended-natural"],
+  { ignores: ["**/*.config.*"] },
   {
+    files: ["**/*.js"],
+    ...tseslint.configs.disableTypeChecked,
+  },
+  {
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
     languageOptions: {
       parserOptions: {
-        project: true,
+        projectService: true,
         tsconfigRootDir: import.meta.dirname,
       },
     },
-  },
-  {
-    files: ["*.js"],
-    ...tseslint.configs.disableTypeChecked,
   },
 );

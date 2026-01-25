758509c16a33b8e03d9b5e58215d6da02fdd82a8
Leif Arriens <leif.arriens@gmail.com>
2026-01-25 18:06:55 +0100

    chore: fix deprecated typescript-eslint config

--- configs/eslint/base.js
+++ configs/eslint/base.js
@@ -1,15 +1,16 @@
 // @ts-check
 
-import { includeIgnoreFile } from "@eslint/compat";
 import * as path from "node:path";
+import { includeIgnoreFile } from "@eslint/compat";
 import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
 import perfectionist from "eslint-plugin-perfectionist";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
+export default defineConfig(
   eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  ...tseslint.configs.stylisticTypeChecked,
+  tseslint.configs.recommendedTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
   perfectionist.configs["recommended-natural"],
   { ignores: ["**/*.config.*"] },
   includeIgnoreFile(path.join(import.meta.dirname, "../../.gitignore")),

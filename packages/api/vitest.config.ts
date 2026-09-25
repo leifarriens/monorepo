920bd816feaf0abbd9bb4d43fa20367d58670761
Leif Arriens <leif.arriens@gmail.com>
2026-09-25 22:02:48 +0200

    fix: coverage reports

--- packages/api/vitest.config.ts
+++ packages/api/vitest.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    coverage: {
+      include: ["src"],
+      reporter: ["text", "text-summary", "lcov"]
+    },
     passWithNoTests: true,
   },
 });
